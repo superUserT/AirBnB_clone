56f933e436b14fd3ecb6bdb337fbd69bc58619bd
superUserT <rantshothabisomail@gmail.com>
2023-10-10 12:55:44 +0200

    base model init

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        """Initialize BaseModel attributes."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance."""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
