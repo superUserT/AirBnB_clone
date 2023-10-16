d4e3e1136c2c5ae25a2f81b1c140a090519bb34a
superUserT <rantshothabisomail@gmail.com>
2023-10-16 23:25:07 +0200

    empty strings in state and city

--- models/city.py
+++ models/city.py
@@ -6,5 +6,5 @@ from models.base_model import BaseModel
 class City(BaseModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        state_id = " "
-        name = " "
+        state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -6,4 +6,4 @@ from models.base_model import BaseModel
 class State(BaseModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        name = " "
+        name = ""
