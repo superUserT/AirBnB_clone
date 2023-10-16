a93a24258d0e52c3f88a0c344fd84688b8b63ab7
superUserT <rantshothabisomail@gmail.com>
2023-10-16 23:36:15 +0200

    reworked models

--- models/city.py
+++ models/city.py
@@ -4,7 +4,5 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        state_id = ""
-        name = ""
+    state_id = ""
+    name = ""
