cd8d257e20b592ed6260bae538ea374f0fb89578
superUserT <rantshothabisomail@gmail.com>
2023-10-17 04:42:53 +0200

    class attributes

--- models/user.py
+++ models/user.py
@@ -5,11 +5,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-
     def __init__(self, *args, **kwargs):
         """Initialize User attributes and call the parent constructor"""
         super().__init__(*args, **kwargs)
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
